a158a34a001059500d7bd41fcf377487238a8865
MarkdeKwaasteniet <markdekwaasteniet@gmail.com>
2025-12-15 16:05:21 +0100

    strange

---
+++ tests/test_features.py
@@ -0,0 +1,27 @@
+import pytest
+import pandas as pd
+from pandas.testing import assert_series_equal
+
+from animal_shelter import features
+
+def test_check_has_name():
+    s = pd.Series(["Ivo", "Henk", "unknown"])
+    result = features.check_has_name(s)
+    expected = pd.Series([True, True, False])
+    assert_series_equal(result, expected)
+
+def test_check_is_dog():
+    s = pd.Series(["dog", "cat", "dog"])
+    result = features.check_is_dog(s)
+    expected = pd.Series([True, False, True])
+    assert_series_equal(result, expected)
+
+def test_sex():
+    s = pd.Series(["Female", "Male"])
+    result = features.get_sex(s)
+    expected = pd.Series(["female", "male"])
+    assert_series_equal(result, expected)
+
+def test_for_exceptions():
+    with pytest.raises(RuntimeError) as exception:
+        features.check_is_dog(pd.Series(["Parrot"]))
