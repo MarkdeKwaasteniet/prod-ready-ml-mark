1b4a1f6a59b9fa2a1e75c84e534d6926a0dbad11
MarkdeKwaasteniet <markdekwaasteniet@gmail.com>
2025-12-15 16:11:06 +0100

    commit test

--- tests/test_features.py
+++ tests/test_features.py
@@ -4,16 +4,20 @@ from pandas.testing import assert_series_equal
 
 from animal_shelter import features
 
+@pytest.fixture()
+def list_of_animals():
+    return pd.Series(["dog", "cat", "dog", "cat", "dog"])
+
 def test_check_has_name():
     s = pd.Series(["Ivo", "Henk", "unknown"])
     result = features.check_has_name(s)
     expected = pd.Series([True, True, False])
     assert_series_equal(result, expected)
 
-def test_check_is_dog():
-    s = pd.Series(["dog", "cat", "dog"])
+def test_check_is_dog(list_of_animals):
+    s = list_of_animals
     result = features.check_is_dog(s)
-    expected = pd.Series([True, False, True])
+    expected = pd.Series([True, False, True, False, True])
     assert_series_equal(result, expected)
 
 def test_sex():
