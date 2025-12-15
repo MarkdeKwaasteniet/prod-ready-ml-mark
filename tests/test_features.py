f4ce6fc4f37777d0f73eca8efa48b78069abcc48
MarkdeKwaasteniet <markdekwaasteniet@gmail.com>
2025-12-15 16:48:26 +0100

    change

--- tests/test_features.py
+++ tests/test_features.py
@@ -7,6 +7,12 @@ from pandas.testing import assert_series_equal
 from animal_shelter import features
 
 
+@pytest.fixture  # type: ignore
+def list_of_animals() -> pd.Series:
+    """Fixture with a list of animals."""
+    return pd.Series(["dog", "cat", "dog", "cat", "dog"])
+
+
 def test_check_has_name() -> None:
     """Test the check_has_name function."""
     s = pd.Series(["Ivo", "Henk", "unknown"])
@@ -15,9 +21,9 @@ def test_check_has_name() -> None:
     assert_series_equal(result, expected)
 
 
-def test_check_is_dog() -> None:
+def test_check_is_dog(list_of_animals: pd.Series) -> None:
     """Test the check_is_dog function."""
-    result = features.check_is_dog(pd.Series(["dog", "cat", "dog", "cat", "dog"]))
+    result = features.check_is_dog(list_of_animals)
     expected = pd.Series([True, False, True, False, True])
     assert_series_equal(result, expected)
 
